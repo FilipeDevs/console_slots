c6400f59d7f1d991ca071acd9645937ce696a16c
Filipe Pereira <filipe.pereira.dev@gmail.com>
2024-02-25 20:49:24 +0100

    finish mini project

--- main.py
+++ main.py
@@ -7,20 +7,56 @@ MIN_BET = 10
 ROWS = 3
 COLS = 3
 
+# How many symbols of each type will be in the slot machine
 symbols_count = {
     "A": 2,
+    "B": 3,
+    "C": 4,
+    "D": 10
+}
+
+# How much each symbol is worth
+symbols_value = {
+    "A": 5,
     "B": 4,
-    "C": 6,
-    "D": 8
+    "C": 3,
+    "D": 2
 }
 
 
+def check_winnings(columns, lines, bet, values):
+    """
+    Check how much the player won and on which lines
+    """
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        # Get the first symbol of the line (transversal line)
+        symbol = columns[0][line]
+        for column in columns:
+            # Check if the symbol is the same in the whole line
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        # Will execute if the for loop does not break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+
+    return winnings, winning_lines
+
+
 def get_slot_machine_spin(rows, cols, symbols):
+    """
+    Generate a slot machine spin. 
+    """
     all_symbols = []
-    for symbol, symbol_count in symbols_count.items():
+    # Create a list with all the symbols
+    for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
             all_symbols.append(symbol)
 
+    # Create the columns of the slot machine spin by randomly choosing a symbol from the list
     columns = []
     for _ in range(cols):
         column = []
@@ -33,10 +69,14 @@ def get_slot_machine_spin(rows, cols, symbols):
 
         columns.append(column)
 
+    # Return the columns, this is a matrix of the slot machine spin but in transposed form (rows are columns and columns are rows)
     return columns
 
 
 def print_slot_machine(columns):
+    """
+    Print the slot machine spin in a human readable format (transposed matrix)
+    """
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             print(column[row], end=" | ") if i != len(
@@ -91,10 +131,9 @@ def get_bet():
     return amount
 
 
-def main():
-    balance = deposit()
+def spin(balance):
+    lines = get_number_of_lines()
     while True:
-        lines = get_number_of_lines()
         bet = get_bet()
         total_bet = bet * lines
 
@@ -109,6 +148,22 @@ def main():
 
     slots = get_slot_machine_spin(ROWS, COLS, symbols_count)
     print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbols_value)
+    print(f"You won : {winnings} $")
+    print(f"You won on lines :", *winning_lines)
+    return winnings - total_bet
+
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is : {balance} $")
+        answer = input("Presse enter to play. (q to quit)")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You left with : {balance}$")
 
 
 main()
